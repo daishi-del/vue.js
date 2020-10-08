2a3fed2edcf8b55084aeea068f1228dda7e8cfd3
室奈々美 <nanami@muronanami-no-MacBook-Pro.local>
2020-10-08 17:25:35 +0900

    add new path

--- src/router/index.js
+++ src/router/index.js
@@ -15,15 +15,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
- {
+  {
     path: '/form',
     name: 'Form',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Form.vue')
-  } 
-    
+    component: () => import('../views/Form.vue')
+  }  
 ]
 
 const router = createRouter({
